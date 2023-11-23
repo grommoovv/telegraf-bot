9e9b94258e6ec6fabe2ebdc47535120cdbfe649f
GEORGE GROMOV <gromov.dev13@gmail.com>
2023-11-23 08:35:17 +0300

    create config service

---
+++ src/config/config.ts
@@ -0,0 +1,30 @@
+import { config, DotenvParseOutput } from 'dotenv'
+
+interface IConfigService {
+  get(key: string): string
+}
+
+class ConfigService implements IConfigService {
+  private config: DotenvParseOutput
+
+  constructor() {
+    const { error, parsed } = config()
+    if (error) {
+      throw new Error('Не найден файл .env')
+    }
+    if (!parsed) {
+      throw new Error('Пустой файл .env')
+    }
+    this.config = parsed
+  }
+
+  get(key: string): string {
+    const res = this.config[key]
+    if (!res) {
+      throw new Error('Пустой файл .env')
+    }
+    return res
+  }
+}
+
+export { ConfigService, IConfigService }
